68cc1ad64024db88d86ee4787b35c78ae99f0e4f
kmhbDOes <kmhbipul@gmail.com>
2023-05-08 01:13:58 +0600

    Displayed all cards on home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useLoaderData } from "react-router-dom";
 import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
 
 function App() {
+  const coffee = useLoaderData();
   return (
-    <>
-      <h1 className="text-6xl text-purple-600">Coffee Maker</h1>
-    </>
+    <div className="m-20">
+      <h1 className="text-6xl text-purple-600 text-center">
+        Hot Cold Coffee: {coffee.length}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {coffee.map((c) => (
+          <CoffeeCard key={c._id} c={c}></CoffeeCard>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,8 +1,54 @@
+import Swal from "sweetalert2";
+
 const AddCoffee = () => {
+  const handleAddCoffee = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+    console.log(newCoffee);
+
+    //   Send Data to server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "User Added successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h3 className="text-3xl font-extrabold">Add Coffee</h3>
-      <form>
+      <form onSubmit={handleAddCoffee}>
         {/* form name and quantity Row */}
         <div className="md:flex mb-8">
           <div className="form-control md:w-1/2">
