8b9be4a97e5f3d0090baed79ad765d52da104a4f
kmhbDOes <kmhbipul@gmail.com>
2023-05-08 14:43:49 +0600

    Update coffee page updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const coffee = useLoaderData();
   return (
     <div className="m-20">
-      <h1 className="text-6xl text-purple-600 text-center">
+      <h1 className="text-6xl text-purple-600 text-center my-20">
         Hot Cold Coffee: {coffee.length}
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,16 +1,60 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ c }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = c;
+  const { _id, name, quantity, supplier, taste, category, details, photo } = c;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your Coffee has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure>
         <img src={photo} alt="Movie" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">New movie is released!</h2>
-        <p>Click the button to watch on Jetflix app.</p>
+      <div className="flex justify-between items-center w-full pr-4">
+        <h2 className="card-title">Name: {name}</h2>
+        <div>
+          <p>{quantity}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
+        </div>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Watch</button>
+          <div className="btn-group btn-group-vertical space-y-4">
+            <button className="btn">View</button>
+            <Link to={`updateCoffee/${_id}`}>
+              <button className="btn">Edit</button>
+            </Link>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-orange-500"
+            >
+              X
+            </button>
+          </div>
         </div>
       </div>
     </div>
